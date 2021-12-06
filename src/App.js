f3f3334ea92ff8c4b0ebcc062c8e6b86027cfee5
xvferdy <xv.ferdy@gmail.com>
2021-12-06 19:03:15 +0800

    Desktop

--- src/App.js
+++ src/App.js
@@ -2,21 +2,61 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Navigation from "./components/Navigation";
 import Sidebar from "./components/Sidebar";
+import introImg from "./assets/images/desktop/image-interactive.jpg";
 
 import "./stylesheets/css/main.css";
+import Card from "./components/Card";
 
 function App() {
 	return (
 		<div className="loopstudios">
+			{/* HERO */}
 			<div className="hero">
-				<Navigation />
-				<div className="hero__title">
-					<h1>Immersive experiences that deliver</h1>
+				<div className="hero-wrapper">
+					<Navigation />
+					<div className="hero__title">
+						<h1>Immersive experiences that deliver</h1>
+					</div>
 				</div>
 			</div>
-			<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
-			<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
-			<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+			{/* INTRO */}
+			<div className="intro">
+				<div className="intro-wrapper">
+					<img src={introImg} alt="" />
+					<div className="intro__text">
+						<h2 className="intro__text-title">The Leader In Interactive VR</h2>
+						<p className="intro__text-desc">
+							Founded in 2011, Loopstudios has been producing world-class
+							virtual reality projects for some of the best companies around the
+							globe. Our award-winning creations have transformed businesses
+							through digital experiences that bind to their brand.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			{/* CREATIONS */}
+			<div className="creations">
+				<div className="creations-wrapper">
+					<div className="creations__header">
+						<h2 className="creations__header-title">Our creations</h2>
+						<button className="creations__header-btn">See All</button>
+					</div>
+					<div className="creations__cards">
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+					</div>
+				</div>
+			</div>
+
+			{/* FOOTER */}
+			<Footer />
 		</div>
 	);
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,64 @@
 import React from "react";
+import brand from "../assets/images/logo.svg";
+import instagram from "../assets/images/icon-instagram.svg";
+import pinterest from "../assets/images/icon-pinterest.svg";
+import twitter from "../assets/images/icon-twitter.svg";
+import facebook from "../assets/images/icon-facebook.svg";
 
 function Footer() {
-	return <div>footer</div>;
+	return (
+		<footer className="footer">
+			<div className="footer-container">
+				<div className="footer__left">
+					<img className="brand" src={brand} alt="" />
+					<ul>
+						<li>
+							<a href="">About</a>
+						</li>
+						<li>
+							<a href="">Careers</a>
+						</li>
+						<li>
+							<a href="">Events</a>
+						</li>
+						<li>
+							<a href="">Products</a>
+						</li>
+						<li>
+							<a href="">Support</a>
+						</li>
+					</ul>
+				</div>
+				<div className="footer__right">
+					<ul>
+						<li>
+							<a href="">
+								<img src={instagram} alt="" />
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src={pinterest} alt="" />
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src={twitter} alt="" />
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src={facebook} alt="" />
+							</a>
+						</li>
+					</ul>
+					<div className="copyright">
+						© 2021 Loopstudios. All rights reserved.{" "}
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
